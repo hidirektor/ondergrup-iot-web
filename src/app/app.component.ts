3ef091b5000c53f24fea29cbf2e7c562de07c05f
Halil İbrahim Direktör <hidirektor@gmail.com>
2024-08-13 13:03:55 +0300

    Version Control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
-import { Title } from '@angular/platform-browser';
+import {Component, OnInit} from '@angular/core';
+import {NavigationEnd, Router, RouterOutlet} from '@angular/router';
+import {Title} from '@angular/platform-browser';
 
-import { IconSetService } from '@coreui/icons-angular';
-import { iconSubset } from './icons/icon-subset';
+import {IconSetService} from '@coreui/icons-angular';
+import {iconSubset} from './icons/icon-subset';
+import {CookieService} from "ngx-cookie-service";
 
 @Component({
   selector: 'app-root',
@@ -14,10 +15,13 @@ import { iconSubset } from './icons/icon-subset';
 export class AppComponent implements OnInit {
   title = 'ÖnderGrup IoT Yönetim Paneli';
 
+  private appVersion = '13.08.24-2';
+
   constructor(
     private router: Router,
     private titleService: Title,
-    private iconSetService: IconSetService
+    private iconSetService: IconSetService,
+    private cookieService: CookieService
   ) {
     this.titleService.setTitle(this.title);
     // iconSet singleton
@@ -30,5 +34,14 @@ export class AppComponent implements OnInit {
         return;
       }
     });
+    const storedVersion = this.cookieService.get('appVersion');
+    if (storedVersion !== this.appVersion) {
+      this.clearCookies();
+      this.cookieService.set('appVersion', this.appVersion);
+    }
+  }
+
+  private clearCookies() {
+    this.cookieService.deleteAll('/');
   }
 }
